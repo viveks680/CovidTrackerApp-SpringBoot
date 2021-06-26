c98ac30da4863cbcf7c3f49cb81f40251c95c2cb
VivekSenapati <viveks680@gmail.com>
2021-06-26 16:44:56 -0400

    First commit CovidTrackerApp

---
+++ src/main/java/com/vs680/CovidTrackerApp/controllers/HomeController.java
@@ -0,0 +1,29 @@
+package com.vs680.CovidTrackerApp.controllers;
+
+import com.vs680.CovidTrackerApp.models.LocationStats;
+import com.vs680.CovidTrackerApp.services.CovidDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    CovidDataService coronaVirusDataService;
+
+    @GetMapping("/")
+    public String home(Model model) {
+        List<LocationStats> allStats = coronaVirusDataService.getAllStats();
+        int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
+        model.addAttribute("locationStats", allStats);
+        model.addAttribute("totalReportedCases", totalReportedCases);
+        model.addAttribute("totalNewCases", totalNewCases);
+
+        return "home";
+    }
+}
---
+++ src/test/java/com/vs680/CovidTrackerApp/CovidTrackerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vs680.CovidTrackerApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidTrackerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
